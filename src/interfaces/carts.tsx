dff099e23c3c28c3c61bad89cf1326ceb8acf044
Dan013577947 <93627888+Dan013577947@users.noreply.github.com>
2025-12-07 13:38:20 +0800

    included interface for Cart Products

---
+++ src/interfaces/carts.tsx
@@ -0,0 +1,28 @@
+
+export interface CartProduct {
+  discountPercentage:number;
+  discountedTotal:number;
+  id:number;
+  price:number;
+  quantity:number;
+  thumbnail:string;
+  title:string;
+  total:number;
+}
+
+export interface Cart {
+  discountedTotal: number;
+  id: number;
+  product: CartProduct[];
+  total:number;
+  totalProducts:number;
+  totalQuanity:number;
+  userId:number;
+}
+
+export interface CartList {
+  carts: Cart[];
+  total: number;
+  skip: number;
+  limit: number;
+}
--- src/pages/cart/Cart.tsx
+++ src/pages/cart/Cart.tsx
@@ -1,6 +1,16 @@
 import type { AddToCartProps } from "../../interfaces/addToCartAmount";
 import Header from "../../components/Header";
+import { useEffect } from "react";
+import axios from "axios";
 export default function Cart({totalAddToCartAmount, setTotalAddToCartAmount}: AddToCartProps) {
+  
+  useEffect(()=>{
+    const fetchCartData = async () => {
+      const response = await axios.get('https://dummyjson.com/carts')
+      console.log(response.data)
+    }
+    fetchCartData()
+  },[])
   return (
     <div>
       <Header
